5f193efe90e5e88b7260ae31805713bc67a4c1eb
Michael Gratzl <michael.gratzl@gmx.net>
2017-11-19 18:03:40 +0100

    test

--- src/BusinessList.js
+++ src/BusinessList.js
@@ -8,7 +8,7 @@ class BusinessList extends React.Component {
 			<div className="BusinessList">
 				{this.props.businesses.map(business =>
 					{
-					return <Business business={this.businesses.id} />
+					return <Business business={business} key={business.id} /> 
 					})
 				}
 			</div>
