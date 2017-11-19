e64f188ddd71a19da33f7342f6b998538486e603
Michael Gratzl <michael.gratzl@gmx.net>
2017-11-19 19:14:38 +0100

    exchanged App.js

--- src/App.js
+++ src/App.js
@@ -7,23 +7,27 @@ import Yelp from './Yelp.js';
 class App extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       businesses: []
     };
+
     this.searchYelp = this.searchYelp.bind(this);
   }
+
   searchYelp(term, location, sortBy) {
     Yelp.search(term, location, sortBy).then(businesses => {
       this.setState({businesses: businesses});
     });
   }
+
   render() {
     return (
-<div className="App">
-  <h1>ravenous</h1>
-  <SearchBar searchYelp={this.searchYelp} />
-<BusinessList businesses={this.state.businesses} /> 
-</div>
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} />
+      </div>
     );
   }
 }
