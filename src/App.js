f2d56297b70674daff3088f9f3536bcbcd53a625
Michael Gratzl <michael.gratzl@gmx.net>
2017-11-19 17:56:35 +0100

    fixed issue this.searchYelp is undefined

--- src/App.js
+++ src/App.js
@@ -4,40 +4,18 @@ import './App.css';
 import BusinessList from './BusinessList.js';
 import SearchBar from './SearchBar.js';
 import Yelp from './Yelp.js';
-
-/*const business = {
-	imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-	name: 'MarginOtto Pizzeria',
-	address: '1010 Paddington Way',
-	city: 'Flavortown',
-	state: 'NY',
-	zipCode: '10101',
-	category: 'Italian',
-	rating: 4.5,
-	reviewCount: 90
-}
-
-const businesses = [
- business, 
- business,
- business,
- business,
- business,
- business
-]
-*/
-class App extends Component {
-  constructor(props){
-	  super(props);
-	  this.state = {
-		  businesses: []
-	  }
-  this.searchYelp.bind(this);
-  const searchYelp = (term, location, sortBy) => {
-	  Yelp.search(term, location, sortBy).then(businesses =>{
-		  this.setState({businesses: businesses});
-	  });
-  };
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      businesses: []
+    };
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+  searchYelp(term, location, sortBy) {
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({businesses: businesses});
+    });
   }
   render() {
     return (
